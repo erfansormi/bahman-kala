5da822ee0608e8c62afd3eeef8753919a66a0b40
Erfan <erfansormi97@gmail.com>
2025-07-12 17:34:40 +0330

    fixed some bugs

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,13 +1,18 @@
+import LoadingScreen from "@/components/common/loading-screen";
+import { useIsAuthenticated } from "@/hooks/auth/useIsAuthenticated";
 import { Redirect, Slot } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useIsAuthenticated } from "@/hooks/auth/useIsAuthenticated";
 
 export default function Layout() {
-  const { token } = useIsAuthenticated();
+  const { token, isLoading } = useIsAuthenticated();
   const isGuest = !token;
 
   if (isGuest) {
     return <Redirect href="/auth/login" />;
+  }
+
+  if (isLoading) {
+    return <LoadingScreen />;
   }
 
   return (
